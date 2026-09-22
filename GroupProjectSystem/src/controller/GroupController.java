7acefdba67c867b339025dbbf4ae8e963cceaf7c
Skywalker909 <kilashomoshood114@gmail.com>
2026-09-22 10:07:54 +0100

    Add files via upload

--- GroupProjectSystem/src/controller/GroupController.java
+++ GroupProjectSystem/src/controller/GroupController.java
@@ -2,6 +2,7 @@ package controller;
 import model.Group;
 import model.Student;
 import model.DataManager;
+import java.util.ArrayList;
 
 public class GroupController {
 	private DataManager dataManager;
@@ -9,4 +10,25 @@ public class GroupController {
 	public GroupController (DataManager dataManager) {
 		this.dataManager = dataManager;
 	}
-	
+	// create new group
+	public boolean addGroup(Group group){
+		for (Group existingGroup : dataManager.getGroups()){
+			if (existingGroup.getGroupName().equals(group.getGroupName())){
+				return false;
+			}
+		}
+		dataManager.addGroup(group);
+		return true;
+	}
+	// edit groups
+	public boolean editGroup(String groupName,String description){
+		for (Group group: dataManager.getGroupName()){
+			if (group.getGroupName().equals(groupName)){
+				group.setGroupName(groupName);
+				group.setDescription(description);
+				return true;
+			}
+		}
+		return false;
+	}
+	// 
